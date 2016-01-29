b6237282cb55e836df6c53b817167b3ea0d20fbf
unknown <BSmagacz@VAN-SBS-BSMAG.corp.isacorp.com>
2016-01-29 10:24:54 -0800

    Added checkmate/stalemate functions

--- public/chess/controllers/chess.client.controller.js
+++ public/chess/controllers/chess.client.controller.js
@@ -1,114 +1,81 @@
 angular.module('chess').controller('ChessController', ['$scope', 'Socket',
-	function($scope, Socket) {
+    function($scope, Socket) {
 
-		$scope.moves = [];
+        $scope.moves = [];
 
-		Socket.on('move', function(move) {
-    		game.move(move);
-    		board.position(game.fen());
-    		$scope.moves.push(move);
-		});
+        Socket.on('move', function(move) {
+            game.move(move);
+            board.position(game.fen());
+            $scope.moves.push(move);
+        });
 
-		Socket.on('message', function(message) {
-    		$scope.messages.push(message);
-		});
+        // do not pick up pieces if the game is over
+        var onDragStart = function(source, piece, position, orientation) {
+            if (game.game_over() === true ||
+                (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+                (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+                return false;
+            }
+        };
 
-		// do not pick up pieces if the game is over
-		var onDragStart = function(source, piece, position, orientation) {
-		  if (game.game_over() === true ||
-		      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-		      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-		    return false;
-		  }
-		};
+        var onDrop = function(source, target) {
 
-		var onDrop = function(source, target) {
-			
-			var move = game.move({
-				from: source, 
-				to: target,
-				promotion: 'q'
-			});
+            var move = game.move({
+                from: source,
+                to: target,
+                promotion: 'q'
+            });
 
-			var message = {
-				text: 'HELLLLLOOOOO'
-			}
+            if (move === null) {
+                return 'snapback';
+            } else {
+                updateStatus();
+                Socket.emit('move', move);
+            }
+        };
 
-			if (move === null) {
-				return 'snapback';
-			} else {
-				updateStatus();
-				Socket.emit('move', move);
-			}
-		};
+        var cfg = {
+            draggable: true,
+            position: 'start',
+            onDragStart: onDragStart,
+            onDrop: onDrop
+        };
 
-		var cfg = {
-			draggable: true,
-			position: 'start',
-			onDragStart: onDragStart,
-			onDrop: onDrop
-		};
+        var board = new ChessBoard('gameBoard', cfg);
+        var game = new Chess();
 
-		var board = new ChessBoard('gameBoard', cfg);
-		var game = new Chess();
+        var updateStatus = function() {
 
-		var updateStatus = function() {
-		  /*var status = '';
+            var gameOver = game.in_checkmate();
+            var stalemate = game.in_draw();
+            var check = game.in_check();
 
-		  var moveColor = 'White';
-		  if (game.turn() === 'b') {
-		    moveColor = 'Black';
-		  }*/
+            var status = '';
 
-		  var gameOver = game.in_checkmate();
-		  var stalemate = game.in_draw();
-		  
-		  if (gameOver) {
-		  	status = 'Game over, stalemate';
-		    Socket.emit('gameOver', gameOver);
-		  }
+            if (gameOver) {
+                status = 'Game over, checkmate';
+                alert (status);
+                Socket.emit('gameOver', gameOver);
+            } else if (stalemate) {
+                status = 'Game over, stalemate';
+                Socket.emit('stalemate', stalemate);
+            } else if (check) {
+            	status = 'You are in check';
+            	Socket.emit('check', check);
+            }
+        };
 
-		  else if (stalemate) {
-		    status = 'Game over, stalemate';
-		    Socket.emit('stalemate', stalemate);
-		  }
+        var toggleOrientation = function() {
 
-		  // game still on
-		  /*else {
-		    status = moveColor + ' to move';
+            if (board.orientation() == 'white') {
+                board.orientation('black');
+            } else {
+                board.orientation('white');
+            }
+        };
 
-		    // check?
-		    if (game.in_check() === true) {
-		      status += ', ' + moveColor + ' is in check';
-		    }
-		  }*/
-
-		  //statusEl.html(status);
-		  //fenEl.html(game.fen());
-		  //pgnEl.html(game.pgn());
-		};
-
-		var toggleOrientation = function() {
-			
-			if (board.orientation()=='white') {
-				board.orientation('black');
-			} else {
-				board.orientation('white');
-			}
-		};
-
-		$scope.flipBoard = function() {
-			toggleOrientation();
-		};
-
-		$scope.sendMessage = function() {
-			var message = {
-				text: this.messageText
-			}
-
-			Socket.emit('message', message);
-
-			this.messageText = '';
-		};
-	}
+        $scope.flipBoard = function() {
+            toggleOrientation();
+        };
+    }
 ]);
